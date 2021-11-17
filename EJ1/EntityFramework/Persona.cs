1e403f55a4ccf8cb9fb9c68eee34e11f7fa08cb0
MARTIN RODRIGO LOPEZ <lopezmr@o365frcuutneduar.onmicrosoft.com>
2021-11-17 19:22:53 -0300

    Inicio

---
+++ EJ1/EntityFramework/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkTP4
+{
+    class Persona
+    {
+        public int PersonaId { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public List<Telefono> Telefonos { get; set; }
+    }
+}
---
+++ EJ1/EntityFramework/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkTP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AgendaContext())
+            {
+                //Alta
+                Persona mPersona = new Persona
+                {
+                    PersonaId = 1,
+                    Nombre = "Juan",
+                    Apellido = "Sánchez",
+                    Telefonos = new List<Telefono>
+                    {
+                        new Telefono
+                        {
+                            TelefonoId = 1,
+                            Numero = "555 - 123456",
+                            Tipo = "Celular"
+                          }
+                    }
+                };
+
+                db
+            }
+
+
+
+        }
+    }
+}
