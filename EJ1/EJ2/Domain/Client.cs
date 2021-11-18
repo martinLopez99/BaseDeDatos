fcc8c2a5d45ef3ca37a5924621a6d19b0fb34521
MARTIN RODRIGO LOPEZ <lopezmr@o365frcuutneduar.onmicrosoft.com>
2021-11-18 16:26:45 -0300

    EJ2 Carpeta Domain lista

---
+++ EJ1/EJ2/Domain/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ2.Domain
+{
+    class Client
+    {
+
+        List<Account> Accounts = new List<Account>();
+
+        Document ClientDocument = new Document();
+
+        public int IdCliente {  get;   set; }
+
+        public string FirstName { get ;   set; }
+
+        public string LastName { get ;   set; }
+    }
+}  
---
+++ EJ1/EJ2/Domain/DocumentType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ2.Domain
+{
+    public enum DocumentType
+    {
+        DNI = 1,
+        CUIL = 2,
+        LE = 3,
+        LC = 4
+    }
+}
