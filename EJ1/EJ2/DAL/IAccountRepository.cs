6f3266d6754adfc75a5eaeb47924740ca80f6787
MARTIN RODRIGO LOPEZ <lopezmr@o365frcuutneduar.onmicrosoft.com>
2021-11-19 18:02:46 -0300

    Revert "Acceso a la carpeta Domain solucionado"
    This reverts commit 459284c0044ffbca5edeb62e252e1e12650ef29f.

--- EJ1/EJ2/DAL/IAccountRepository.cs
+++ EJ1/EJ2/DAL/IAccountRepository.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace AccountManager.DAL
 {
-    public interface IAccountRepository
+    public interface IAccountRepository: IRepository<>
     {
-        public double GetAccountBalance(AccountManager.Domain.Account pAccount);
+        public double GetAccountBalance(Account pAccount);
 
-        public IEnumerable<AccountManager.Domain.Account> GetOverdrawnAccounts();
+        public IEnumerable<Account> GetOverdrawnAccounts();
 
-        public IEnumerable<AccountManager.Domain.AccountMovement> GetLastMovements(AccountManager.Domain.Account pAccount, int pCount = 7);
+        public IEnumerable<AccountMovement> GetLastMovements(Account pAccount, int pCount = 7);
     }
 }
